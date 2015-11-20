7078df948faa747de4508a48225e46aa7dffff1f
bulld0zer <sergey.khablyuk@gmail.com>
2015-11-20 10:25:31 +0200

    version - set fix

--- src/silhouette.cpp
+++ src/silhouette.cpp
@@ -14,6 +14,8 @@
 
 using namespace std;
 
+
+
 class sPoint {
     public :
         sPoint(){}
@@ -28,149 +30,147 @@ class sPoint {
             return y;
         }
         bool operator < (const sPoint &point) const {
-            return (point.x < x) || (point.y < y);
+            return (point.y < y) || (point.x < x) ;
         }
-//        bool operator() (const sPoint* lhs, const sPoint* rhs) {
-//            return (lhs->x == rhs->x) && (lhs->y == rhs->y) ;
+//        ~sPoint() {
+//            delete this;
 //        }
-//        ostream& operator<< (ostream& stream ) {
-//          return stream << "[" << x << ", " << y << "]";
-//        }
+
     private:
         double x;
         double y;
 };
 
 
-void printSet(Vector< Set<sPoint> > silhouettes, GBufferedImage* image) {
+void printSet(Set< Set<sPoint>* > silhouettes, GBufferedImage* image) {
     int i = 0;
-    long colorSet[3] = {0x0000ff, 0xFF0000, 0x00FF00};
-    for (Set<sPoint> set : silhouettes) {
-        cout << "count = " << set.size() << endl;
-        long color = colorSet[i++ % 3];
-        for (sPoint p : set) {
+    long colorSet[4] = {0x0000ff, 0xFF0000, 0x00FF00, 0xffff00};
+    for (Set<sPoint>* set : silhouettes) {
+        cout << "count = " << set->size() << endl;
+        long color = colorSet[i++ % 4];
+        for (sPoint p : *set) {
             image->setRGB(p.getX(), p.getY(), color);
         }
+        pause(200);
     }
     cout << endl;
 }
 
 bool isBlack(GBufferedImage* image, double x, double y) {
     if (image->inBounds(x, y)) {
-//        cout << image->getRGBString(x, y) << endl;
-//        cout << image->getRGB(x, y) << endl;
         return image->getRGB(x, y) < 3355443;
     }
 
     return false;
 }
 
-bool isInSet(Vector<sPoint> &set, sPoint pt) {
-    if (set.isEmpty()) {
-        return false;
-    }
+void checkPoint(sPoint p, Set< Set<sPoint>* >& mainSet, Grid<bool>& grid, GBufferedImage* image ) {
 
-    for (sPoint var : set) {
-        if (var.getX() == pt.getX() && var.getY() == pt.getY()) {
-            return true;
-        }
-    }
+    Set< Set<sPoint>* > sets;
 
-    return false;
-}
-
-void checkPoint(sPoint p, Vector< Set<sPoint> >& silhouettes, GBufferedImage* image) {
-//    printSet(silhouettes);
-    for (int dx = -1; dx <= 1; dx++) {
+    for (int dx = -1; dx < 1; dx++) {
         for (int dy = -1; dy <= 1; dy++) {
-
-//            if (dx == 0 && dy == 0) {
-//                continue;
-//            }
+            if ((dx == 0 && dy == 0) || (dx == 0 && dy == 1)) {
+                continue;
+            }
             double findX = p.getX() + dx;
             double findY = p.getY() + dy;
 
-            if (isBlack(image, findX, findY)) {
+//            cout << findX << " - " << findY << endl;
+
+
+            if (grid.inBounds(findY, findX) && grid[findY][findX] == 1) {
                 sPoint pt(findX, findY);
-
-                image->setRGB(findX, findY, 0xff0000);
-//                pause(100);
+//                cout << "x = " << findX << ", y = " << findY << endl;
 
 
-//                Stack< Set<sPoint> > sets;
-                for (Set<sPoint> &set : silhouettes) {
+//                image->setRGB(findX, findY, 0x00ff00);
 
-                    if (set.contains(pt)) {
-                        set.add(p);
-//                        sets.add(set);
-                        image->setRGB(p.getX(), p.getY(), 0x0000ff);
-
-//                        return;
+                for (Set<sPoint>* set : mainSet) {
+                    if (set->contains(pt)) {
+                        if (sets.isEmpty()) {
+                            set->add(p);
+                            sets.add(set);
+                        } else {
+                            if (!sets.contains(set)) {
+                                image->setRGB(findX, findY, 0x00ff00);
+                                sets.add(set);
+                            }
+                        }
                     }
                 }
-
-//                while (sets.size() > 1) {
-//                    Set<sPoint> s1 = sets.pop();
-//                    Set<sPoint> s2 = sets.pop();
-
-//                    silhouettes.add(s1 + s2);
-
-//                    s1.clear();
-//                }
+//                pause(50);
+                image->setRGB(p.getX(), p.getY(), 0xcccccc);
+//                image->setRGB(findX, findY, 0xcccccc);
             }
         }
     }
 
-    Set<sPoint> silhouette;
-    image->setRGB(p.getX(), p.getY(), 0x00ff00);
-    silhouette.add(p);
-    silhouettes.add(silhouette);
+//    cout << endl;
+
+//    cout << "sets.size = " << sets.size() << endl;
+//    cout << "sets = " << sets << endl;
+//    cout << "mainSet.size = " << mainSet.size() << endl;
+
+    if (!sets.isEmpty()) {
+        if (sets.size() > 1) {
+//            cout << "mainSet.size = " << mainSet.size() << endl;
+//            cout << "sets.size = " << sets.size() << endl;
+//            pause(1000);
+            Set<sPoint>* sFirst;
+            for (Set<sPoint>* s : sets) {
+                if (sFirst == s) {
+                    continue;
+                }
+                *sFirst += *s;
+                mainSet.remove(s);
+                image->setRGB(p.getX(), p.getY(), 0xffbb66);
+            }
+            mainSet.add(sFirst);
+        }
+    } else {
+        Set<sPoint>* silhouette = new Set<sPoint>;
+        image->setRGB(p.getX(), p.getY(), 0xff0000);
+        silhouette->add(p);
+        mainSet.add(silhouette);
+    }
 }
 
 int main() {
     GWindow gw;
-
     GBufferedImage* image = new GBufferedImage;
     image->load("images/silhouette1.jpg");
-
     gw.setCanvasSize(image->getWidth(), image->getHeight());
     gw.add(image);
 
-    double count = 0;
-
-//    Set<GPoint> silhouette;
-
-    Vector< Set<sPoint> > silhouettes;
-
-    Grid<bool> grid(image->getWidth(), image->getHeight(), 0);
-
-    for (double h = 0; h < image->getHeight(); ++h) {
-//        cout << h << endl;
-        for (double w = 0; w < image->getWidth(); ++w) {
-//            grid.set(h, w, (isBlack(image, w, h)) ? 1 : 0);
-            if (isBlack(image, w, h)) {
-                grid[w][h] = 1;
-
-//                count++;
-
-//                sPoint p(w, h);
-
-//                checkPoint(p, silhouettes, image);
-
+    Grid<bool> grid(image->getHeight(), image->getWidth(), 0);
+    for (double row = 0; row < image->getHeight(); ++row) {
+        for (double col = 0; col < image->getWidth(); ++col) {
+            if (isBlack(image, col, row)) {
+                grid[row][col] = 1;
             }
-//            image->setRGB(w, h, 0xffcc33);
-//            pause(1);
         }
     }
 
-    cout << grid.toString2D() ;
+    Set< Set<sPoint>* > mainSet;
+    for (double col = 0; col < grid.width(); ++col) {
+        for (double row = 0; row < grid.height(); ++row) {
+            if (grid[row][col] == 1) {
+                sPoint p(col, row);
+                checkPoint(p, mainSet, grid, image);
+            }
+        }
+    }
 
-//    printSet(silhouettes, image);
+//    Set<sPoint>* set1 = mainSet.first();
+//    for (Set<sPoint>* set : mainSet) {
+//        if (!(*set1 * *set)->isEmpty()) {
+//            *set1 += *set;
+//        }
+//    }
 
-//    cout << "count = " << silhouettes << endl;
-
-//    cout << "count = " << silhouettes.size() << endl;
-
+    printSet(mainSet, image);
+    cout << "count = " << mainSet.size() << endl;
     delete image;
 
     return 0;
